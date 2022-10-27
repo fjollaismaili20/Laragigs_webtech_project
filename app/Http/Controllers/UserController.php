97c461c2ebfa92986354eae6774d0b9f57673be6
fjollaismaili20 <73657793+fjollaismaili20@users.noreply.github.com>
2022-10-27 21:22:05 +0200

    Register/Login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use PDO;
+
+class UserController extends Controller
+{
+    //Show register/Create Form
+
+    public function create(){
+        return view('user.register');
+    }
+
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required|confirmed|min:6'
+
+        ]);
+
+        //hash password
+         // Hash Password
+         $formFields['password'] = bcrypt($formFields['password']);
+
+         // Create User
+        $user = User::create($formFields);
+
+        // Login
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User created and logged in');
+
+    }
+
+    public function logout(Request $request) {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/')->with('message', 'You have been logged out!');
+
+    }
+        // Show Login Form
+        public function login() {
+            return view('user.login');
+        }
+}
